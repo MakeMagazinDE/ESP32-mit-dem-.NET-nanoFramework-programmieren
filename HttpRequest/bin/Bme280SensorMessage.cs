ed6f928c4637da79b7ae265941a401d1856573e7
Make Magazin <mail@make-magazin.de>
2023-05-14 22:43:18 +0200

    Add files via upload

---
+++ HttpRequest/bin/Bme280SensorMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace HttpRequest
+{
+    internal class Bme280SensorMessage
+    {
+        public Bme280SensorMessage(double temperature, double pressure, double humidity)
+        {
+            Temperature = temperature;
+            Pressure = pressure;
+            Humidity = humidity;
+        }
+
+        public double Temperature { get; private set; }
+
+        public double Pressure { get; private set; }
+
+        public double Humidity { get; private set; }
+    }
+}
